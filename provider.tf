cccab3295306803f509353cb619bb978049cb940
Connor Spiess <connor@spiess.us>
2021-12-26 12:51:38 -0600

    add provider

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+  }
+}
